735a5def31bef8183af6811f1c1be0ed5ed20015
melanydesirima <melanydesirimrim@gmail.com>
2017-06-22 08:55:03 +0700

    versi 1 bismillah

---
+++ application/views/d_profil.php
@@ -0,0 +1,13 @@
+ <div class="span9">
+		<ul class="breadcrumb wellwhite">
+			<li><a href="<?=base_URL()?>">Beranda</a> <span class="divider">/</span></li>
+			<li>Profil Perpustakaan </li>
+		</ul>
+		
+		<div class="span12 wellwhite" style="margin-left: 0px">
+		<div class="row-fluid">
+		<?=$data->profil?>		
+		
+        </div>
+		</div>        
+</div><!--/span-->
